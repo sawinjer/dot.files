4acb406f9698cb59d84af167bd858da5c3860409
sawinjer <apolishchuk@externe.europ-assistance.com>
2025-12-08 03:27:55 -0800

    testing

---
+++ .config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,16 @@
+return {
+	"nvim-neotest/neotest",
+	dependencies = {
+		"nvim-neotest/nvim-nio",
+		"nvim-lua/plenary.nvim",
+		"antoinemadec/FixCursorHold.nvim",
+		"nvim-treesitter/nvim-treesitter",
+	},
+	keys = {
+		{ "<leader>tr", "<cmd>Neotest run<cr>" },
+		{ "<leader>ti", "<cmd>Neotest output<cr>" },
+		{ "<leader>ts", "<cmd>Neotest summary<cr>" },
+		{ "<leader>tp", "<cmd>NeotestPlaywrightPreset<cr>" },
+		{ "<leader>ta", "<cmd>lua require('neotest').run.run({ suite = true })<cr>" },
+	},
+}
